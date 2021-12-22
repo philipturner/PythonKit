9adcc6675c82bd8e35d60dbb74649a82c837d53c
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 13:29:09 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "PythonKit",
-            type: .dynamic
+            type: .dynamic,
             targets: ["PythonKit"]
         )
     ],
